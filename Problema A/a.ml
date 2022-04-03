5c2b4fe19fae42e66d6edd1ac91f6e6b209344f8
RiseGhost <91985039+RiseGhost@users.noreply.github.com>
2022-04-03 21:30:01 +0100

    Update a.ml

--- Problema A/a.ml
+++ Problema A/a.ml
@@ -1,19 +1,17 @@
+(*ocamlfind ocamlopt -linkpkg -package zarith -package str -package unix a.ml -o a && time ./tp*)
 let count = ref 0 (*-> Endereço de memória onde fica guadado o número de chamadas da função*) 
 let rec schroder x =
-  count := !count + 1;
+  count :=  !count + 1;
   if x = 0. then
   1.
   else
   if x = 1. then
   2.
   else
-    ( (((6.*.x -. 3.)/.(x +. 1.)) *. schroder (x -. 1.)) -. ((x -. 2.)/.(x +. 1.)) *. schroder (x -. 2.) )
-
+    ( ((6.*.x -. 3.) *. schroder (x -. 1.)) -. ((x -. 2.) *. schroder (x -. 2.) ) ) /. (x +. 1.)
 
 let s x = 
   int_of_float ( schroder (float_of_int (x)) )
-
-(*5+4+3+2+1 + 4+3+2+1*)
 
 let y =  ref 0 (*Endereço de memória utilizado como variável auxiliar para efetuar a soma*)
 let rec schroder1 x = 
@@ -25,34 +23,30 @@ let rec schroder1 x =
     if x = 1 then
       2
     else
-      if x = 2 then
-        3 * schroder1 (x-1)
-      else
-        (
-          for i = 1 to (x - 2) do
+          (for i = 1 to (x - 2) do
             y := schroder1 i * schroder1 (x-i-1) + !y
           done;
-          3 * schroder1 (x-1) + !y
-        )
-        
+          3 * schroder1 (x-1) + !y)
 
+open Z
+
+(*Z.big_int = schroder3 x*)
+let ss = ref []
+let rec schroder3 x =
+  if x = Z.of_float 0. then
+    ss := Z.of_float 1. :: !ss
+  else
+  if x = Z.of_float 1. then
+    ss := Z.of_float 2. :: !ss
+  else
+    ss := ( ((Z.of_float 6. * x - Z.of_float 3.) * (List.nth !ss 0)) - ((x - Z.of_float 2.) * (List.nth !ss 1) ) ) / (x + Z.of_float 1.) :: !ss;
+  List.nth !ss 0
 
 let () = 
-    for i = 1  to 22 do
-      print_string ("Schroder ");
+    for i = 0  to 10000 do
+      print_string ("Schroder");
       print_int (i);
-      print_string ("->");
-      print_int ( s i );
-      print_string (" ->");
-      print_int !count;
-      print_newline ();
-      count := 0;
-      print_string ("Schroder1 ");
-      print_int (i);
-      print_string ("->");
-      print_int (schroder1 i);
-      print_string (" ->");
-      print_int !count;
-      print_newline ();
-      count := 0
+      print_string (" -> ");
+      print_string ( Z.to_string ( schroder3 ( Z.of_int i ) ) );
+      print_newline();
     done;
